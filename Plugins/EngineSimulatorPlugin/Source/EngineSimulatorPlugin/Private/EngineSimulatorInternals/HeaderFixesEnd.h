9e858a77131986e3ada58eddd5a5716924915c29
Kees <me@keestucker.com>
2025-03-11 20:18:32 +1300

    chore: Add EngineSimulatorPlugin as a git submodule

--- Plugins/EngineSimulatorPlugin/Source/EngineSimulatorPlugin/Private/EngineSimulatorInternals/HeaderFixesEnd.h
+++
@@ -1,9 +0,0 @@
-#if PLATFORM_WINDOWS
-#include "Windows/PostWindowsApi.h"
-#include "Windows/HideWindowsPlatformAtomics.h"
-#include "Windows/HideWindowsPlatformTypes.h"
-
-__pragma(pop_macro("PI"))
-__pragma(pop_macro("TWO_PI"))
-
-#endif
