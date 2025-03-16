12469238ea4fad218a9d943773439f90b36db4c5
Kees <me@keestucker.com>
2025-03-16 15:48:36 +1300

    feat: HOLY SHIT IT ACTUALLY WORKS (my collision resolver)

--- Source/bikegame/Components/KPhysics.cpp
+++ Source/bikegame/Components/KPhysics.cpp
@@ -5,7 +5,7 @@
 
 
 // Sets default values for this component's properties
-UKPhysics::UKPhysics()
+UKPhysics::UKPhysics(): Target(nullptr), Velocity(), AngularVelocity()
 {
 	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
 	// off to improve performance if you don't need them.
@@ -33,107 +33,198 @@ void UKPhysics::TickComponent(float DeltaTime, ELevelTick TickType, FActorCompon
     {
         return;
     }
-	
-    Velocity += GetWorld()->GetGravityZ() * FVector(0.f, 0.f, 1.0f) * DeltaTime;
-	
-    for (FHitResult& Hit : Collisions)
+
+    DeltaTime = DeltaTime / static_cast<float>(Substeps);
+    
+    for (int i = 0; i < Substeps; ++i)
     {
-        float Dot = FVector::DotProduct(Velocity, Hit.Normal);
-        if (Dot < 0.0f)
+        // Simple linear damping
+        Velocity -= (Velocity * LinearDamping * DeltaTime);
+        // Simple angular damping
+        AngularVelocity -= (AngularVelocity * AngularDamping * DeltaTime);
+
+        //--------------------------------------
+        // 1) Gravity & Angular Damping
+        //--------------------------------------
+        // Gravity in Unreal is negative Z, so GetWorld()->GetGravityZ() is negative
+        // Use it in a vector
+        Velocity += (GetWorld()->GetGravityZ() * FVector(0.f, 0.f, 1.f)) * DeltaTime;
+
+        //--------------------------------------
+        // 3) Update Orientation from Angular Velocity
+        //--------------------------------------
+        FQuat Orientation = Target->GetComponentQuat();
         {
-        	FVector LocalInertiaDiag = Target->BodyInstance.GetBodyInertiaTensor();
-        	FMatrix InertiaLocal = FMatrix::Identity;
-        	InertiaLocal.M[0][0] = LocalInertiaDiag.X;
-        	InertiaLocal.M[1][1] = LocalInertiaDiag.Y;
-        	InertiaLocal.M[2][2] = LocalInertiaDiag.Z;
-        	FMatrix R = FRotationMatrix::Make(Orientation.Rotator());
-        	FMatrix InertiaWorld = R * InertiaLocal * R.GetTransposed();
-        	FMatrix InertiaWorldInv = InertiaWorld.InverseFast();
-        	
-        	
-            // Debug arrow for the impact normal
-            DrawDebugDirectionalArrow(
-                GetWorld(),
-                Hit.Location,
-                Hit.Location + (Hit.ImpactNormal * 100.f),
-                10.f, 
-                FColor::Red, 
-                false,
-                0.1f
-            );
-        	
-            FVector ParallelComponent = Dot * Hit.Normal;
-            Velocity -= (1.f + Restitution) * ParallelComponent;  // bounce factor
-        	
-            Location -= (Hit.PenetrationDepth * Hit.ImpactNormal);
-        	
-            float Mass = Target->GetMass();
-            FVector CollisionImpulse = -(1.f + Restitution) * ParallelComponent * Mass;
-        	
-            FVector r = (Hit.Location - Location); 
-            FVector Torque = FVector::CrossProduct(r, CollisionImpulse);
-        	FVector Alpha = InertiaWorldInv.TransformVector(Torque);
-        	AngularVelocity += Alpha * DeltaTime;
+            FVector W = AngularVelocity * DeltaTime;
+            float Theta = W.Size();
 
-        	// Friction
-        	
-        	FVector Normal = Hit.Normal;
-        	FVector VelocityNormalPart = (FVector::DotProduct(Velocity, Normal)) * Normal; 
-        	FVector VelocityTangentPart = Velocity - VelocityNormalPart;
+            if (Theta > KINDA_SMALL_NUMBER)
+            {
+                FVector Axis = W / Theta;
+                float halfTheta = 0.5f * Theta;
+                float s = FMath::Sin(halfTheta);
 
-        	FVector FrictionForce = -(FrictionCoefficient) * VelocityTangentPart;
-        	
-        	float MassInv = 1.0f / Mass;
-        	Velocity += (FrictionForce * MassInv) * DeltaTime;
-        	
-        	FVector FrictionTorque = FVector::CrossProduct(r, FrictionForce);
-        	FVector FrictionAlpha = InertiaWorldInv.TransformVector(FrictionTorque);
-        	AngularVelocity += FrictionAlpha * DeltaTime; 
+                FQuat DeltaQ(Axis.X * s, Axis.Y * s, Axis.Z * s, FMath::Cos(halfTheta));
+                Orientation = DeltaQ * Orientation;
+                Orientation.Normalize();
+            }
+        }
+
+        // For linear motion:
+        FHitResult MoveHit;
+        Target->AddWorldOffset(Velocity * DeltaTime, /*bSweep=*/true, &MoveHit);
+        if (MoveHit.bBlockingHit)
+        {
+            ResolveCollision(MoveHit, DeltaTime);
+        }
+
+        // For angular motion:
+        FQuat CurrentQuat = Target->GetComponentQuat();
+        FQuat DeltaQuat   = Orientation * CurrentQuat.Inverse();
+        FHitResult SweepHit;
+        Target->AddWorldRotation(DeltaQuat.Rotator(), /*bSweep=*/true, &SweepHit);
+        if (SweepHit.bBlockingHit)
+        {
+            ResolveCollision(SweepHit, DeltaTime);
         }
     }
+}
 
-    // Clear collision array for next frame
-    Collisions.Empty();
+void UKPhysics::ResolveCollision(FHitResult& Hit, float DeltaTime)
+{
+    FVector COM = Target->GetCenterOfMass();
+    FQuat Orientation = Target->GetComponentQuat();
+    
+    FVector ImpactPoint = Hit.ImpactPoint;
+    FVector r = (ImpactPoint - COM);
+    
+    // Relative velocity at contact
+    FVector vContact = Velocity + FVector::CrossProduct(AngularVelocity, r);
 
-    // 3) Update linear position
-    Location += Velocity * DeltaTime;
+    // Normal velocity component
+    float dot = FVector::DotProduct(vContact, Hit.Normal);
 
-    // 4) Update orientation via quaternion integration
-    //    We assume AngularVelocity is in radians/sec.
-    //    If you’re storing it in degrees/sec, convert here with * (PI/180.f).
+    // Build inertia in world space
+    FVector LocalInertiaDiag = Target->BodyInstance.GetBodyInertiaTensor();
+    FMatrix InertiaLocal = FMatrix::Identity;
+    InertiaLocal.M[0][0] = LocalInertiaDiag.X;
+    InertiaLocal.M[1][1] = LocalInertiaDiag.Y;
+    InertiaLocal.M[2][2] = LocalInertiaDiag.Z;
+    
+    FMatrix R = FRotationMatrix::Make(Orientation);
+    FMatrix InertiaWorld = R * InertiaLocal * R.GetTransposed();
+    FMatrix InertiaWorldInv = InertiaWorld.InverseFast();
+
+    //--------------------------
+    // 2a) Collision (Normal Impulse)
+    //--------------------------
+    if (dot < -KINDA_SMALL_NUMBER)
     {
-        // Angular displacement this frame
-        FVector W = AngularVelocity * DeltaTime; // total rotation in radians
-        float Theta = W.Size();                  // angle magnitude
+        // Compute the effective mass denominator:
+        // Denom = 1/mass + n dot [ (I⁻¹*(r x n)) x r ]
+        FVector r_cross_n = FVector::CrossProduct(r, Hit.Normal);
+        FVector I_inv_r_cross_n = InertiaWorldInv.TransformVector(r_cross_n);
+        FVector rotational_component = FVector::CrossProduct(I_inv_r_cross_n, r);
+        float effectiveMass = (1.0f / Mass) + FVector::DotProduct(Hit.Normal, rotational_component);
+        
+        // Compute the impulse magnitude
+        float j = -(1.f + Restitution) * dot / effectiveMass;
+        
+        // Impulse vector along the normal
+        FVector CollisionImpulse = j * Hit.Normal;
+        
+        // Update linear velocity
+        Velocity += CollisionImpulse / Mass;
 
-    	if (Theta > KINDA_SMALL_NUMBER)
-    	{
-    		FVector Axis = W / Theta;
-    		float HalfTheta = 0.5f * Theta;
-    		float s = FMath::Sin(HalfTheta);
+        DrawDebugDirectionalArrow(
+            GetWorld(),
+            ImpactPoint,
+            ImpactPoint + CollisionImpulse,
+            10.f,
+            FColor::Red,
+            false,
+            0.1f
+        );
 
-    		FQuat DeltaQ(Axis.X * s, Axis.Y * s, Axis.Z * s, FMath::Cos(HalfTheta));
-    		Orientation = DeltaQ * Orientation;
-    		Orientation.Normalize();
-    	}
+        // Update angular velocity using the impulse torque:
+        // Torque = r x CollisionImpulse, and Δω = I⁻¹ * Torque
+        FVector Torque = FVector::CrossProduct(r, CollisionImpulse);
+        FVector AngularImpulse = InertiaWorldInv.TransformVector(Torque);
+        AngularVelocity += AngularImpulse;
     }
 
-    // 5) Apply final transform
-    Target->SetWorldLocation(Location);
-    // If your “Target” is e.g. a StaticMeshComponent, it accepts a quaternion:
-    Target->SetWorldRotation(Orientation);
+    //--------------------------
+    // 2b) Static + Coulomb Friction
+    //--------------------------
+    // Tangential velocity
+    FVector Normal = Hit.Normal;
+    FVector vN = FVector::DotProduct(vContact, Normal) * Normal;
+    FVector vT = vContact - vN;  // tangential component
+    float vTmag = vT.Size();
 
-	Target->SetPhysicsLinearVelocity(Velocity);
-	Target->SetPhysicsAngularVelocityInRadians(AngularVelocity);
+    // We'll do a simple approach: assume normal force = mass * g
+    // (Ignoring slopes or multiple collisions.)
+    float normalForce = Mass * -GetWorld()->GetGravityZ() * 
+               FMath::Abs(FVector::DotProduct(Hit.Normal, FVector::UpVector));
+    
+    // If tangential speed is extremely small, attempt static friction
+    // We'll compute how much friction is needed to bring tangential velocity to zero in one frame.
+    // frictionNeeded = - (Mass * vT) / DeltaTime
+    // If that is <= mu_s * normalForce, we remain fully at rest. Otherwise, we apply dynamic friction.
+    FVector frictionForce = FVector::ZeroVector;
+    if (vTmag < KINDA_SMALL_NUMBER)
+    {
+        // Probably (nearly) at rest in the tangential direction
+        float maxStaticFriction = StaticFrictionCoefficient * normalForce;
+
+        // Force needed to zero tangential velocity this frame
+        FVector frictionNeeded = -(Mass * vT) / (DeltaTime * Substeps);
+        float frictionNeededMag = frictionNeeded.Size();
+
+        if (frictionNeededMag <= maxStaticFriction)
+        {
+            // We can stay completely at rest
+            frictionForce = frictionNeeded; 
+        }
+        else
+        {
+            // Exceeds static friction => use dynamic friction
+            // Magnitude = mu_k * normalForce, direction opposite vT
+            // Safeguard against zero length
+            if (vTmag > SMALL_NUMBER)
+            {
+                frictionForce = -(DynamicFrictionCoefficient * normalForce) * (vT / vTmag);
+            }
+        }
+    }
+    else
+    {
+        // Sliding => dynamic friction
+        float maxDynamicFriction = DynamicFrictionCoefficient * normalForce;
+        frictionForce = -(maxDynamicFriction) * (vT / vTmag);
+    }
+
+    // Apply friction (force) to linear velocity
+    Velocity += (frictionForce / Mass) * DeltaTime;
+
+    // Draw friction arrow (optional). Scaling it so arrow length ~ friction impulse
+    DrawDebugDirectionalArrow(
+        GetWorld(),
+        ImpactPoint,
+        ImpactPoint + frictionForce * (0.01f),  // reduce for visibility
+        10.f, 
+        FColor::Green, 
+        false,
+        0.1f
+    );
+
+    // Apply friction torque to angular velocity
+    FVector frictionTorque = FVector::CrossProduct(r, frictionForce);
+    FVector frictionAlpha = InertiaWorldInv.TransformVector(frictionTorque);
+    AngularVelocity += frictionAlpha * DeltaTime;
 }
 
 void UKPhysics::AsyncPhysicsTickComponent(float DeltaTime, float SimTime)
 {
 	Super::AsyncPhysicsTickComponent(DeltaTime, SimTime);
-}
-
-void UKPhysics::OnHitCallback(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitiveComponent* OtherComp,
-	FVector NormalImpulse, const FHitResult& Hit)
-{
-	Collisions.Add(Hit);
 }
--- Source/bikegame/Components/KPhysics.h
+++ Source/bikegame/Components/KPhysics.h
@@ -19,25 +19,47 @@ public:
 protected:
 	// Called when the game starts
 	virtual void BeginPlay() override;
+	
 	UPROPERTY(BlueprintReadWrite)
 	UPrimitiveComponent* Target;
+
 	UPROPERTY(EditAnywhere, BlueprintReadWrite)
-	float FrictionCoefficient = 5.0f;
-	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	int Substeps = 32;
+	
+	// Mass (in kg or your chosen units)
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Physics")
+	float Mass = 1.f;
+
+	// Static friction coefficient (mu_s) - friction that can hold an object at rest
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Friction")
+	float StaticFrictionCoefficient = 0.6f;
+
+	// Dynamic friction coefficient (mu_k) - friction once it's sliding
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Friction")
+	float DynamicFrictionCoefficient = 0.5f;
+
+	// Coefficient of restitution (bounciness)
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Physics")
 	float Restitution = 0.5f;
+
+	// Simple linear damping factor
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Physics")
+	float LinearDamping = 0.1f;
+	
+	// Simple angular damping factor
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Physics")
+	float AngularDamping = 0.1f;
 
 public:	
 	// Called every frame
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 	virtual void AsyncPhysicsTickComponent(float DeltaTime, float SimTime) override;
-	UFUNCTION(BlueprintCallable)
-	void OnHitCallback(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit);
-
+	
 private:
+	void ResolveCollision(FHitResult& Hit, float DeltaTime);
+	
 	FVector Velocity;
 	FVector AngularVelocity;
-	FVector Location = FVector(0.f, 0.f, 1000.f);
-	FQuat Orientation = FQuat::Identity;
 	
 	TArray<FHitResult> Collisions;
 };
